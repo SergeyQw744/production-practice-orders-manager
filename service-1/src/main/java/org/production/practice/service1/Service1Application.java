7265c198ca48cf8eedec36f7194ffe20bad3ac10
111 <sergey.streltsov2004@mail.ru>
2026-04-17 22:28:29 +0300

    Initial commit

---
+++ service-1/src/main/java/org/production/practice/service1/Service1Application.java
@@ -0,0 +1,14 @@
+package org.production.practice.service1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@SpringBootApplication
+@EnableKafka
+public class Service1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+}
