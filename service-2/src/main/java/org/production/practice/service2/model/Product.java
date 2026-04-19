bee16d0fe878d015898b8ef1478a2f29e6d6183d
111 <sergey.streltsov2004@mail.ru>
2026-04-19 20:06:59 +0300

    add-persistence

---
+++ service-2/src/main/java/org/production/practice/service2/model/Product.java
@@ -0,0 +1,34 @@
+package org.production.practice.service2.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    @Column(length = 255, nullable = false)
+    private UUID id;
+
+    @Column(length = 255, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int count;
+
+    @Column(name = "date_time_last_change")
+    @UpdateTimestamp
+    private LocalDateTime dateTimeLastChange;
+}
---
+++ service-2/src/main/java/org/production/practice/service2/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package org.production.practice.service2.repository;
+
+import org.production.practice.service2.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
